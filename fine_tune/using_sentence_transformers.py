e853528f648ba2574c5d397c860f6380b176a51a
mohamad-tohidi <the.mohammad.tohidi@gmail.com>
2025-09-29 12:17:02 +0330

    sample data

--- fine_tune/using_sentence_transformers.py
+++ fine_tune/using_sentence_transformers.py
@@ -13,7 +13,7 @@ from sentence_transformers import (
 from sentence_transformers.evaluation import EmbeddingSimilarityEvaluator
 
 
-data_files="dataset_collection/data/raw_es_results.csv"
+data_files="dataset_collection/data/all_es_data.csv"
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print("Using device:", device)
 
@@ -24,52 +24,54 @@ model = SentenceTransformer(model_name, device=device, trust_remote_code=True)
 
 train_dataset = load_dataset("csv", data_files=data_files, cache_dir=None)
 
-print(train_dataset["train"].column_names) 
+# print(train_dataset["train"].column_names) 
 
-# with open("dataset_collection/data/evaluation_data.json", "r", encoding="utf-8") as ef:
-#     eval_items = json.load(ef)
-# eval_qs = []
-# eval_ps = []
-# eval_scores = []
-# for item in eval_items:
-#     q = item.get("question", "").strip()
-#     rels = item.get("relevant_questions", [])
-#     for rel in rels:
-#         p = rel.get("content", "").strip()
-#         rank = rel.get("rank", 1)
-#         score = max(0.0, 5.0 - 0.5 * (rank - 1))
-#         if q and p:
-#             eval_qs.append(q)
-#             eval_ps.append(p)
-#             eval_scores.append(score)
-# evaluator = EmbeddingSimilarityEvaluator(
-#     eval_qs, eval_ps, eval_scores, name="val_q_rel_pairs"
-# )
+with open("dataset_collection/data/evaluation_data.json", "r", encoding="utf-8") as ef:
+    eval_items = json.load(ef)
+eval_qs = []
+eval_ps = []
+eval_scores = []
+for item in eval_items:
+    q = item.get("question", "").strip()
+    rels = item.get("relevant_questions", [])
+    for rel in rels:
+        p = rel.get("content", "").strip()
+        rank = rel.get("rank", 1)
+        score = max(0.0, 5.0 - 0.5 * (rank - 1))
+        if q and p:
+            eval_qs.append(q)
+            eval_ps.append(p)
+            eval_scores.append(score)
 
 
-# train_loss = losses.MultipleNegativesRankingLoss(model)
+evaluator = EmbeddingSimilarityEvaluator(
+    eval_qs, eval_ps, eval_scores, name="val_q_rel_pairs"
+)
 
-# args = SentenceTransformerTrainingArguments(
-#     gradient_accumulation_steps=16,
-#     per_device_train_batch_size=8,
-#     per_device_eval_batch_size=8,
-#     num_train_epochs=3,
-#     save_strategy="steps",
-#     save_steps=500,
-#     eval_strategy="steps",
-#     load_best_model_at_end=True,
-#     metric_for_best_model="eval_loss",
-#     greater_is_better=False,
-#     save_total_limit=1,
-#     output_dir="fine_tune/output/gte-multilingual-base-finetuned-using-simcse",
-# )
 
-# trainer = SentenceTransformerTrainer(
-#     model=model,
-#     args=args,
-#     train_dataset=train_dataset,
-#     evaluator=evaluator,
-#     loss=train_loss,
-# )
+train_loss = losses.MultipleNegativesRankingLoss(model)
 
-# trainer.train()
+args = SentenceTransformerTrainingArguments(
+    gradient_accumulation_steps=16,
+    per_device_train_batch_size=8,
+    per_device_eval_batch_size=8,
+    num_train_epochs=3,
+    save_strategy="steps",
+    save_steps=500,
+    eval_strategy="steps",
+    load_best_model_at_end=True,
+    metric_for_best_model="eval_loss",
+    greater_is_better=False,
+    save_total_limit=1,
+    output_dir="fine_tune/output/gte-multilingual-base-finetuned-using-simcse",
+)
+
+trainer = SentenceTransformerTrainer(
+    model=model,
+    args=args,
+    train_dataset=train_dataset,
+    evaluator=evaluator,
+    loss=train_loss,
+)
+
+trainer.train()
