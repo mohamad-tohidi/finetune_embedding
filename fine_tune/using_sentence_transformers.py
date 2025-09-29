a558e7343f88f9636f222c19b7614ef211d3fe1b
mohamad-tohidi <the.mohammad.tohidi@gmail.com>
2025-09-29 13:30:53 +0330

    nothing

--- fine_tune/using_sentence_transformers.py
+++ fine_tune/using_sentence_transformers.py
@@ -3,7 +3,6 @@ import json
 import torch
 from datasets import load_dataset
 from sentence_transformers import (
-    InputExample,
     SentenceTransformer,
     SentenceTransformerTrainer,
     SentenceTransformerTrainingArguments,
@@ -99,7 +98,7 @@ args = SentenceTransformerTrainingArguments(
     eval_strategy="steps",
     eval_steps=500,
     load_best_model_at_end=True,
-    metric_for_best_model="eval_val_q_rel_pairs_spearman_cosine",
+    metric_for_best_model="eval_loss",
     greater_is_better=True,
     save_total_limit=1,
     output_dir=output_dir,
